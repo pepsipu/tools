919a0b26b95e2f30d0d51ff0ac70a4cd9319f9a2
Sammy Hajhamid <40325571+Pepsipu@users.noreply.github.com>
2018-09-23 19:55:38 -0700

    uselessbuttonsv1

---
+++ uselessbuttonsv1/UBv1.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from uselessbuttonsfunctions import GUITest
+import time
+print("loading up uselessbuttonsv1...")
+time.sleep(1)
+root = Tk()
+
+GUITest = GUITest(root)
+
+root.mainloop()
